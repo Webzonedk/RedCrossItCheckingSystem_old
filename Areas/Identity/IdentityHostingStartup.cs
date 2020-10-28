f421457ae987c955474363cff2ad130e19439e74
Kent Pedersen <webzone@kpdesign.dk>
2020-10-28 09:59:32 +0100

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using udlejningsboliger.Areas.Identity.Data;
+using udlejningsboliger.Data;
+
+[assembly: HostingStartup(typeof(udlejningsboliger.Areas.Identity.IdentityHostingStartup))]
+namespace udlejningsboliger.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<udlejningsboligerDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("udlejningsboligerDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<udlejningsboligerDbContext>();
+            });
+        }
+    }
+}
